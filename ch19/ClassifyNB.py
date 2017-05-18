53f8ad44a7cf50e9dc2bfede3fb39ca0bec00269
Mike Maraya <mike.maraya@gmail.com>
2017-05-17 22:11:35 -0400

    completed test

--- ch19/ClassifyNB.py
+++ ch19/ClassifyNB.py
@@ -8,5 +8,8 @@ def classify(features_train, labels_train):
                         
                             
 ### your code goes here!
-                                    
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    return clf
                                         
